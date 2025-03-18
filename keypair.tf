f6409747f4b380503975bb6fcbb368850a13854c
ykcoda <yawkyereh@outlook.com>
2025-03-18 01:09:30 +0000

    updated the aws_key_pair to use interpolation config file

--- keypair.tf
+++ keypair.tf
@@ -6,7 +6,7 @@ resource "tls_private_key" "ec2_private_key" {
 
 
 resource "aws_key_pair" "ec2_keypair" {
-  key_name   = var.key_name
+  key_name   = "${var.env}-${var.key_name}"
   public_key = tls_private_key.ec2_private_key.public_key_openssh   
   
     tags = {
