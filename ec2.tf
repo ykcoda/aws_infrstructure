d9e7ecfa39ee947fd2c50b23fedbd06291cf1b88
ykcoda <yawkyereh@outlook.com>
2025-03-18 02:03:03 +0000

    Security group config updated

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web_server" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.dev_subnet_1.id
     associate_public_ip_address = true
-    vpc_security_group_ids = aws_security_group.my_security_group.id
+    vpc_security_group_ids = [aws_security_group.my_security_group.id]
     key_name = aws_key_pair.ec2_keypair.key_name
     tags = {
         Name = "${var.project}-${var.env}-Web-Server"
@@ -16,7 +16,7 @@ resource "aws_instance" "db_server" {
     instance_type = "t2.micro"
     subnet_id = aws_subnet.dev_subnet_2.id
     associate_public_ip_address = true
-    vpc_security_group_ids = aws_security_group.my_security_group.id
+    vpc_security_group_ids = [aws_security_group.my_security_group.id]
     key_name = aws_key_pair.ec2_keypair.key_name
     tags = {
         Name = "${var.project}-${var.env}-DB-Server"
