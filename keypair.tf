c15b820a3a7f42b5226a61c29d1f6451c7319fcb
ykcoda <yawkyereh@outlook.com>
2025-03-18 01:39:17 +0000

    variable created for public key

--- keypair.tf
+++ keypair.tf
@@ -1,10 +1,7 @@
-# This file creates a key pair for EC2 instances and saves the private key to a local file
-# The public key is loaded from a local file
-# The private key is saved to a local file
-# The key pair is tagged with the project name and environment name
+
 resource "aws_key_pair" "ec2_keypair" {
   key_name   = "${var.env}-${var.key_name}"
-  public_key = file("${path.module}/my-terraform-ec2-key.pub")  #Load public key from local file
+  public_key = var.public_key
   
     tags = {
         Name = "${var.project}-${var.env}-keypair"
