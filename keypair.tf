32498193ba191656b35b803d9540b160b349300d
ykcoda <yawkyereh@outlook.com>
2025-03-18 01:29:36 +0000

    updated path of public key to an absolute path

--- keypair.tf
+++ keypair.tf
@@ -4,7 +4,7 @@
 # The key pair is tagged with the project name and environment name
 resource "aws_key_pair" "ec2_keypair" {
   key_name   = "${var.env}-${var.key_name}"
-  public_key = file("~/.ssh/my-terraform-ec2-key.pub")  #Load public key from local file
+  public_key = file("/Users/ykafreh/.ssh/my-terraform-ec2-key.pub")  #Load public key from local file
   
     tags = {
         Name = "${var.project}-${var.env}-keypair"
