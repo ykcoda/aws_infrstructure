e7fcca31bf38b3c314a8ed3be8983000f5149059
ykcoda <yawkyereh@outlook.com>
2025-03-18 02:18:30 +0000

    Security group updated to allow all traffic on egress

--- securitygroup.tf
+++ securitygroup.tf
@@ -2,7 +2,10 @@ resource "aws_security_group" "my_security_group" {
     name        = "${var.project}-${var.env}-security-group"
     description = "Allow inbound traffic on port 22 and 80"
     vpc_id      = aws_vpc.dev_vpc.id
-    
+    tags = {
+        Name = "${var.project}-${var.env}-security-group"
+    }
+
     ingress {
         from_port   = 0
         to_port     = 0
@@ -10,10 +13,9 @@ resource "aws_security_group" "my_security_group" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
-    #ingress{
-    #    from_port   = -1
-    #    to_port     = -1
-    #    protocol    = "icmp"
-    #  cidr_blocks = ["0.0.0.0/0"]
-    #}
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
 }
