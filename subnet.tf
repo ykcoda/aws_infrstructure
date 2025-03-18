35810e92d4bc651aa900e8641139b470a0c9c518
ykcoda <yawkyereh@outlook.com>
2025-03-18 03:24:00 +0000

    updated subnet to allow public ip

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "dev_subnet_1" {
   vpc_id            = aws_vpc.dev_vpc.id
   cidr_block        = var.subnet_1
   availability_zone = "us-east-1a"
-
+  map_public_ip_on_launch =  true
   tags = {
     Name = "${var.project}-${var.env}-Subnet-1"
   }
@@ -12,7 +12,7 @@ resource "aws_subnet" "dev_subnet_2" {
   vpc_id            = aws_vpc.dev_vpc.id
   cidr_block        = var.subnet_2
   availability_zone = "us-east-1b"
-
+  map_public_ip_on_launch = true
   tags = {
     Name = "${var.project}-${var.env}-Subnet-2"
   }
